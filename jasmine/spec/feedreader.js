842f19093eb1c572bad77ca9642c48bd495f13b1
Abdu Assabri <abdusabri@outlook.com>
2018-09-09 16:32:28 +0200

    Add test that there is at least one entry in the feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -94,22 +94,33 @@ $(function() {
             expect(body.classList
                 .contains('menu-hidden')).toBe(true);
         });
+    });
        
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {    
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+        
+        it('have at least one entry in the feed container', function(done) {
+            expect(document.querySelector('.feed').hasChildNodes())
+                .toBe(true);
+            done();
+        });
+    });    
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
+        
+    
 }());
